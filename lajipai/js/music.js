a16e57644a4b8a8530c5974ce2cf7c56a5afbd72
zxlv119 <191858926@qq.com>
2023-05-09 15:26:02 +0800

    修正

--- lajipai/js/music.js
+++ lajipai/js/music.js
@@ -103,8 +103,8 @@ function music() {
             var insert = '';
             insert += '<a style="float:left;background: url(./image/music/082022.png) -6px -6px no-repeat;" class="off">1<div class="bd"><p style="width: 55px;">区域场景I</p></div></a>';
             insert += '<a style="float:left;background: url(./image/music/082023.png) -6px -6px no-repeat;" class="off">2<div class="bd"><p style="width: 55px;">区域场景II</p></div></a>';
-            insert += '<a style="float:left;background: url(./image/music/082024.png) -6px -6px no-repeat;" class="off">3<div class="bd"><p>迷宫挑战I</p></div></a>';
-            insert += '<a style="float:left;background: url(./image/music/082033.png) -6px -6px no-repeat;" class="off">4<div class="bd"><p>迷宫挑战II</p></div></a>';
+            insert += '<a style="float:left;background: url(./image/music/082024.png) -6px -6px no-repeat;" class="off">3<div class="bd"><p style="width: 55px;">迷宫挑战I</p></div></a>';
+            insert += '<a style="float:left;background: url(./image/music/082033.png) -6px -6px no-repeat;" class="off">4<div class="bd"><p style="width: 55px;">迷宫挑战II</p></div></a>';
             insert += '<a style="float:left;background: url(./image/music/082027.png) -6px -6px no-repeat;" class="off">5<div class="bd"><p>讨伐歼灭</p></div></a>';
             insert += '<a style="float:left;background: url(./image/music/082025.png) -6px -6px no-repeat;" class="off">6<div class="bd"><p style="width: 55px;">大型任务I</p></div></a>';
             insert += '<a style="float:left;background: url(./image/music/082026.png) -6px -6px no-repeat;" class="off">7<div class="bd"><p style="width: 55px;">大型任务II</p></div></a>';
