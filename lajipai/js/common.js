e306f7a777ca51ef1fe3b73f62292711df524c12
susudesu <404375155@qq.com>
2024-09-15 01:57:30 +0800

    风脉更新

--- lajipai/js/common.js
+++ lajipai/js/common.js
@@ -19,7 +19,7 @@ $(function () {
 function load() {
     $('#item').append(
         '<li style="margin-right: 500px;"><a onclick="check()"><img src="image/更新速览.png"><div><p class="buff" style="color: white;">New!!</p></div></a><p>更新速览</p></li>'
-        , '<li><a onclick="aether()"><img style="animation-delay:0.6s;" src="image/风脉.png"><div><p class="buff" style="color: orange;">///</p></div></a><p>风脉</p></li>'
+        , '<li><a onclick="aether()"><img style="animation-delay:0.6s;" src="image/风脉.png"><div><p class="buff" style="color: orange;">Update</p></div></a><p>风脉</p></li>'
         , '<li style="margin-right: 400px;"><a onclick="explore()"><img style="animation-delay:0.7s;" src="image/探索笔记.png"><div><p class="buff" style="color: orange;">Update</p></div></a><p>探索笔记</p></li>'
         , '<li><a onclick="pets()"><img style="animation-delay:0.7s;" src="image/宠物.png"><div><p class="buff" style="color: orange;">Update</p></div></a><p>宠物一览</p></li>'
         , '<li><a onclick="mounts()"><img style="animation-delay:0.8s;" src="image/坐骑.png"><div><p class="buff" style="color: orange;">Update</p></div></a><p>坐骑一览</p></li>'
