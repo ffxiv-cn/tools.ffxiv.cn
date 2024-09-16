9a22ae3c7b7e1e05cb353616dfd0f1377679d72e
zxlv119 <191858926@qq.com>
2024-09-16 21:12:54 +0800

    更新速览更新
    调整好了check
    调整了布局

--- lajipai/js/check.js
+++ lajipai/js/check.js
@@ -17,7 +17,7 @@ function check() {
             '<ul id="page_itemtop"></ul>'
             , '<ul id="page_explain" style="height:100px;"><div id="pagenum" class="pagenum" style="width: 900px;"></div></ul>'
             , '<ul id="page_check" style="height:50px;"><div id="vernum" class="vernum" style="width: 900px;margin-left: 45px;"></div></ul>'
-            , '<ul id="page_item" style="min-height: 540px;display: flex;overflow: hidden;"></ul>'
+            , '<ul id="page_item" style="display: flex;overflow: hidden;"></ul>'
         );
         $('#page_item').css("height", document.documentElement.clientHeight - 400);
         $('#page_itemtop').append(
@@ -101,13 +101,14 @@ function checkinfo(obj) {
 
             csvList = $.csv()(data);
             $('#page_item').empty();
-            $('#page_item').append('<ul class="level1" style=""></ul><ul class="level234" style=""></ul>');
+            // $('#page_item').append('<ul class="level1" style=""></ul><ul class="level234" style=""></ul>');
+            $('#page_item').append('<ul class="level234" style=""></ul>');
             var insert = "";
             for (var i = 1; i < csvList.length; i++) {
-                if (csvList[i][0] == 1) {
+                /* if (csvList[i][0] == 1) {
                     $('#page_item .level1').append("<li><p>" + csvList[i][1] + "</p></li>");
                 }
-                else if (csvList[i][0] == 2) {
+                else */ if (csvList[i][0] == 2) {
                     $('#page_item .level234').append("<li class='level2 nosel'><p style=''>" + csvList[i][1] + "</p><b class='marka'></b><div></div></li>");
                 }
                 else if ((csvList[i][0] == 3 && i == csvList.length - 1) || (csvList[i][0] == 3 && csvList[i + 1][0] != 4)) {
@@ -138,15 +139,15 @@ function checkinfo(obj) {
                 if (csvList[i][2] != "") {
                     $('#page_item .level234 img:last').attr("src", "image/check/" + csvList[i][2]);
                 }
-                if (csvList[i][2] == "" && csvList[i][0] != 2) {
+                if (csvList[i][2] == "" && csvList[i][0] != 2 && csvList[i][0] != 1) {
                     $('#page_item .level234 img:last').attr("src", "image/check/未选中.png");
                     $('#page_item .level234 img:last').bind('click', function () { checksel(this) });
-                    $('#page_item .level234 img:last').parent().addClass("nosel");
+                    $('#page_item .level234 p:last').bind('click', function () { checkselp(this) });
+                    $('#page_item .level234 img:last').parent().addClass("nosel");                    
                 }
                 if (csvList[i][3] != "") {
                     $('#page_item .level234 p:last').addClass(csvList[i][3]);
-                    $('#page_item .level234 p:last').css("text-decoration", "underline");
-                    $('#page_item .level234 p:last').css("color", "rgb(102 155 255)");
+                    $('#page_item .level234 p:last').parent().addClass("zuobiao");
                     $('#page_item .level234 p:last').bind('click', function () { bigmap(this.className) });
                 }
             }
@@ -157,34 +158,38 @@ function checksel(obj) {
     if ($(obj).parent().hasClass("nosel")) {
         $(obj).parent().removeClass("nosel");
         $(obj).parent().addClass("sel");
-        $(obj).attr("src", "image/check/选中.png");
-        if ($(obj).parent().parent().children(".nosel").length > 0) { }
-        else {
-            $(obj).parent().parent().parent().removeClass("nosel");
-            $(obj).parent().parent().parent().addClass("sel");
-            if ($(obj).parent().parent().parent().has("img")) {
-                $(obj).parent().parent().parent().children("img").attr("src", "image/check/选中.png");
+        $(obj).attr("src", "image/check/选中.png");        
+        var level = $(obj).parents(".nosel").length;
+        for (var i = 0; i < level; i++) {
+            // alert($(obj).parents(".nosel:eq(0)").attr("class"));
+            if ($(obj).parents(".nosel:eq(0)").children("div").children(".nosel").length == 0) {
+                $(obj).parents(".nosel:eq(0)").addClass("sel");
+                $(obj).parents(".nosel:eq(0)").children("img").attr("src", "image/check/选中.png");
+                $(obj).parents(".nosel:eq(0)").removeClass("nosel");
             }
         }
-        if ($(obj).parent().children("div").children(".nosel").length > 0) {
-            $(obj).parent().children("div").children(".nosel").children("img").attr("src", "image/check/选中.png");
-            $(obj).parent().children("div").children(".nosel").addClass("sel");
-            $(obj).parent().children("div").children(".nosel").removeClass("nosel");
+        if ($(obj).parent().find(".nosel").length > 0) {
+            $(obj).parent().find(".nosel").children("img").attr("src", "image/check/选中.png");
+            $(obj).parent().find(".nosel").addClass("sel");
+            $(obj).parent().find(".nosel").removeClass("nosel");
         }
     }
     else if ($(obj).parent().hasClass("sel")) {
         $(obj).parent().removeClass("sel");
         $(obj).parent().addClass("nosel");
         $(obj).attr("src", "image/check/未选中.png");
-        if ($(obj).parent().parent().parent().hasClass("sel")) {
-            $(obj).parent().parent().parent().removeClass("sel");
-            $(obj).parent().parent().parent().addClass("nosel");
-            $(obj).parent().parent().parent().children("img").attr("src", "image/check/未选中.png");
+        if ($(obj).parents().hasClass("sel")) {
+            $(obj).parents(".sel").addClass("nosel");
+            $(obj).parents(".sel").children("img").attr("src", "image/check/未选中.png");
+            $(obj).parents(".sel").removeClass("sel");
         }
-        if ($(obj).parent().children("div").children(".sel").length > 0) {
-            $(obj).parent().children("div").children(".sel").children("img").attr("src", "image/check/未选中.png");
-            $(obj).parent().children("div").children(".sel").addClass("nosel");
-            $(obj).parent().children("div").children(".sel").removeClass("sel");
+        if ($(obj).parent().find(".sel").length > 0) {
+            $(obj).parent().find(".sel").children("img").attr("src", "image/check/未选中.png");
+            $(obj).parent().find(".sel").addClass("nosel");
+            $(obj).parent().find(".sel").removeClass("sel");
         }
     }
+}
+function checkselp(obj) {
+    checksel($(obj).parent().children("img"));
 }
